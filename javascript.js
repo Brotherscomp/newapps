1cc33ded997a395b46b6ba828485a6658df1f815
Brotherscomp <yazezewkassa@gmail.com>
2025-06-07 03:06:55 +0300

    add

--- javascript.js
+++
@@ -1,6 +0,0 @@
-const buttons = document.querySelectorAll('button');
-buttons.forEach(btn => {
-  btn.addEventListener('click', () => {
-    console.log('Button clicked:', btn.textContent);
-  });
-});
